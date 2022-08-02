8541392c66555a896357b322bd257747240db0df
sumit desale <108015691+sumitdesale@users.noreply.github.com>
2022-08-02 16:26:18 +0530

    commit fourth

---
+++ DAY-2_Introduction to java/src/com/capgemini/introduction/IntegerRunTimeInput.java
@@ -0,0 +1,27 @@
+package com.capgemini.introduction;
+import java.util.*;
+
+public class IntegerRunTimeInput {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	System.out.println(a);
+	//we use print to print ant statement in same line
+	System.out.print("output  ");
+	
+	 // println is use to print output in next line
+	System.out.println(b);
+	
+	//compile time char input
+	char c='s';
+	System.out.println("compile time input ="+c);
+	
+	//runtime char input
+	char d=sc.next().charAt(0);
+	System.out.println("enter charecter");
+	System.out.println("run time input "+d);
+	}
+
+}
---
+++ DAY-2_Introduction to java/src/com/capgemini/introduction/StringInput.java
@@ -0,0 +1,21 @@
+package com.capgemini.introduction;
+ import java.util.*;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+public class StringInput {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("write sentense");
+		 String a=sc.next();
+		 String b=sc.nextLine();
+		 
+		 // it will print the only first word of sentense 
+		 System.out.println("1st output is ="+a);
+		 // it will print whole sentense
+		 System.out.println("2nd output is ="+b);
+		
+	}
+
+}
