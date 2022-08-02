d7dcfb6b85fd694ba12141a8576a65a16bbb014d
sumit desale <108015691+sumitdesale@users.noreply.github.com>
2022-08-02 15:46:26 +0530

    third commit

---
+++ DAY-2_Introduction to java/src/com/capgemini/introduction/ByteProgram.java
@@ -0,0 +1,12 @@
+package com.capgemini.introduction;
+//program for Byte data type
+public class ByteProgram {
+
+	public static void main(String[] args) {
+		byte a=127;
+		//byte a= 129;
+		// it will give you error bcoz of data type range
+		System.out.println(a);
+	}
+
+}
