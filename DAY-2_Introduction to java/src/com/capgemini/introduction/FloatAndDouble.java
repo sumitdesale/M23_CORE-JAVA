e6e8687f97b9ad064636456c2998a46dcad56e26
sumit desale <108015691+sumitdesale@users.noreply.github.com>
2022-08-03 13:02:35 +0530

    commit fifth

---
+++ DAY-2_Introduction to java/src/com/capgemini/introduction/FloatAndDouble.java
@@ -0,0 +1,24 @@
+package com.capgemini.introduction;
+
+import java.util.*;
+public class FloatAndDouble {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		//compile time input
+		float f= 3.40282347f;
+		double d=4.9406564584;
+		
+		
+		System.out.println("float example ="+f);
+		System.out.println("double example ="+d);
+		
+		// runtime input 
+		System.out.println("enter numbers in point ");
+		float a=sc.nextFloat();
+		System.out.println("float runtime input example ="+a);
+
+	}
+
+}
