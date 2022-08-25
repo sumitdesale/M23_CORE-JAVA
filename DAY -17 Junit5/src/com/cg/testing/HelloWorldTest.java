b81528438fd0a8c84adb1fd189eaf81e201a9ed9
sumit desale <108015691+sumitdesale@users.noreply.github.com>
2022-08-25 13:04:08 +0530

    first commit

---
+++ DAY -17 Junit5/src/com/cg/testing/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.cg.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest implements TestLifeCyclelogger
+{
+	@Test
+void print()
+{
+	
+	System.out.println("Hello World");
+	
+}
+}
