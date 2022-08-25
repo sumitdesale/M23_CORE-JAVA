395f26cd233b4462b2c24b760fb1ba6a5d2bcef7
sumit desale <108015691+sumitdesale@users.noreply.github.com>
2022-08-25 13:03:32 +0530

    first commit

---
+++ DAY -16  Generics/src/com/cg/generics/Sender.java
@@ -0,0 +1,19 @@
+package com.cg.generics;
+
+public class Sender<T>{
+	private T messege;
+//getter method 
+	public T getMessege() {
+		return messege;
+	}
+//setter method 
+	public void setMessege(T messege) {
+		this.messege = messege;
+	}
+public void sendmessege()
+
+{
+System.out.println("messege type: "+getMessege().getClass());
+System.out.println("contect are: "+getMessege());
+
+}}
