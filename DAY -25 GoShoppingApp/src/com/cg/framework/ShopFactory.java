cbac1ea0bff57064feb582107eece0442f6991c1
sumit desale <108015691+sumitdesale@users.noreply.github.com>
2022-09-08 17:58:38 +0530

    first commit

---
+++ DAY -25 GoShoppingApp/src/com/cg/framework/ShopFactory.java
@@ -0,0 +1,7 @@
+package com.cg.framework;
+
+public abstract class ShopFactory 
+{
+	public abstract PrimeAcc getNewPrimeAccount(int AccNo, String accNm, float charges, boolean isPrime);
+	public abstract NormalAcc getNewNormalAccount(int AccNo, String accNm,float charges,float deliveryCharges);
+}
